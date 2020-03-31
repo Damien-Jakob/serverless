808fbab665297e07fb7c7f99a2031d3f4ef25a3f
Damien Jakob <damien.jakob@cpnv.ch>
2020-03-31 09:26:05 +0200

    Add function

--- handler.rb
+++ handler.rb
@@ -1,11 +1,21 @@
 require 'json'
 
-def hello(event:, context:)
-  {
-    statusCode: 200,
-    body: {
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
-    }.to_json
-  }
+def to_upper_case(event:, context:)
+  begin
+    puts "Received Request: #{event}"
+    {
+        statusCode: 200,
+        body: {
+            message: 'Serverless toUpperCase',
+            input: event[:body].upcase
+        }.to_json
+    }
+  rescue StandardError => e
+    puts e.message
+    puts e.backtrace.inspect
+    {
+        statusCode: 400,
+        body: JSON.generate("Bad request, please POST a request body!")
+    }
+  end
 end
