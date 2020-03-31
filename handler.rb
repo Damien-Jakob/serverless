c978ed4943973da0531fb19f33c2aae50dc4bb72
Damien Jakob <damien.jakob@cpnv.ch>
2020-03-31 11:15:19 +0200

    Fix response

--- handler.rb
+++ handler.rb
@@ -5,17 +5,14 @@ def to_upper_case(event:, context:)
     puts "Received Request: #{event}"
     {
         statusCode: 200,
-        body: {
-            message: 'Serverless toUpperCase',
-            input: event.upcase
-        }.to_json
+        body: event["body"].upcase,
     }
   rescue StandardError => e
     puts e.message
     puts e.backtrace.inspect
     {
         statusCode: 400,
-        body: e.message
+        body: e.message,
     }.to_json
   end
 end
